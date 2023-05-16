b652bc94d5d4e1edb6a424259241d382df3f862b
QChown <132530931+QChown@users.noreply.github.com>
2023-05-15 21:33:22 -0600

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <main>
+      <section id='splash'>
+        <div className='deco'></div>
+        <div className='fg'>
+          <h1 className='colossus'>
+            Discover the power of <br /> front-end development.
+          </h1>
+          <p>
+            Quentin Chown: Transforming Designs into Immersive Web Experiences
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default App;
